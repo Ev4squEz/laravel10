bb08c1ac82bb2addf97711f4046f431a7579d35b
Elvis Vasquez S <41029552+Ev4squEz@users.noreply.github.com>
2023-05-25 19:05:24 -0400

    feat:

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -16,6 +16,9 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
     <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
     <style>
+
+
+
       .gradient {
         background: linear-gradient(90deg, #d53369 0%, #daae51 100%);
       }
